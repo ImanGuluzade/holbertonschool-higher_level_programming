254242548a715dfb53ca9377cc34dd53a4aca784
ImanGuluzade <iman.guluzade@gmail.com>
2025-11-29 14:46:31 +0000

    Complete 8-concat_edges.py to print object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# This script prints 'object-oriented programming with Python'
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# using string slicing to get object-oriented programming
 print(str[39:66] + " with Python")
