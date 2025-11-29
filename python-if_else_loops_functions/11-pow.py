98ae1fc368c5bbc8a2c26fda4202aa0c101c6826
ImanGuluzade <iman.guluzade@gmail.com>
2025-11-29 13:47:50 +0000

    Add 11-pow.py and 11-main.py exercises

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Return a to the power of b."""
+    if b == 0:
+        return 1
+    result = 1
+    if b > 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result *= a
+        result = 1 / result
+    return result
