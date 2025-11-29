3583886e8216d4274d566e8e941dd4db674f7f97
ImanGuluzade <iman.guluzade@gmail.com>
2025-11-29 12:16:50 +0000

    Move 9-main.py and 9-print_last_digit.py into python-if_else_loops_functions folder

---
+++ holbertonschool-higher_level_programming/python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """Prints a string in uppercase followed by a new line"""
+    print("{}".format("".join(chr(ord(c)-32) if 'a' <= c <= 'z' else c for c in str)))
