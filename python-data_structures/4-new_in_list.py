516bf564fb84434fba2c7469d99e76f81081536b
ImanGuluzade <iman.guluzade@gmail.com>
2025-12-04 07:57:34 +0000

    Add new_in_list function and main test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace an element at a specific position without modifying the original list."""
+    """Replace an element at a specific position without modifying the list."""
     new_list = my_list[:]  # Create a copy of the list
     if idx < 0 or idx >= len(new_list):
         return new_list
