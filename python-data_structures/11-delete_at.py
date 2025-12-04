c4127d06ed879eb92e627f3bd8264c7002ef6d05
ImanGuluzade <iman.guluzade@gmail.com>
2025-12-04 08:11:50 +0000

    Add divisible_by_2 and delete_at functions with main files

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """Deletes the item at a specific position in a list.
-
-    Args:
-        my_list (list): List of elements
-        idx (int): Index of element to delete
-
-    Returns:
-        list: Updated list
-    """
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[:idx] + my_list[idx + 1:]
+    del my_list[idx]
+    return my_list
