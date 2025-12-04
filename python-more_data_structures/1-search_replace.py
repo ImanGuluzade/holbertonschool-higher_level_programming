1de54211fbb09870533c84ef4c12a3dcd9bd72b2
ImanGuluzade <iman.guluzade@gmail.com>
2025-12-04 08:30:33 +0000

    Add python-more_data_structures folder with initial exercises

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """Returns a new list where all occurrences of 'search' are replaced with 'replace'."""
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
