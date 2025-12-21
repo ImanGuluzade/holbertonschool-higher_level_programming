9e9d33777ea32b77d6804d5a9bac862488258e60
ImanGuluzade <iman.guluzade@gmail.com>
2025-12-21 02:38:18 +0000

    Add SQL script for shows without genres

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,7 @@
--- List all TV shows that have no genre linked
+-- 12-no_genre.sql
+-- List all shows without a genre linked
+-- Database name will be passed as an argument to mysql
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
