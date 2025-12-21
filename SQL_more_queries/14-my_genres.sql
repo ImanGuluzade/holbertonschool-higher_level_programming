e51d542ce45a7d5b0af821904709ea114bc0f346
ImanGuluzade <iman.guluzade@gmail.com>
2025-12-21 02:44:31 +0000

    Fix 14-my_genres.sql query for Dexter genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- List all genres of the show Dexter
-SELECT g.name
-FROM tv_genres g
-JOIN tv_show_genres tg ON g.id = tg.genre_id
-JOIN tv_shows t ON tg.tv_show_id = t.id
-WHERE t.title = 'Dexter'
-ORDER BY g.name ASC;
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
